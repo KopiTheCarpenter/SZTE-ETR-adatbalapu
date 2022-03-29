a805a612e7074274c39ab6f694324a8f0879fb80
Varga Ákos <h053561@stud.u-szeged.hu>
2022-03-30 01:04:30 +0200

    Listázásra alkalmas projekt hiányzó funkciókkal.

--- Views/HTML/listAll.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include_once "../../Controllers/diakokController.php";
-    $controller = new Diakokcontroller();
-    $var = $controller->list_all();
-    $var2 = oci_execute($var);
-    while ( $row = oci_fetch_array($var, OCI_ASSOC+OCI_RETURN_NULLS) ) {
-        echo $row["NEV"] . $row["OKTATASIAZON"] . "\n";
-    }
-    oci_free_statement($var);
